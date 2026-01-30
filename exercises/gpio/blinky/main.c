1cdce50b99f7fcd33a5fe43b76cdba783da702d5
bsgursta <bryansgonzalez156@gmail.com>
2026-01-30 05:18:10 +0000

    gpio exercises

--- exercises/gpio/blinky/main.c
+++ exercises/gpio/blinky/main.c
@@ -5,18 +5,14 @@
 
 //Get Device Tree Node Identifier
 #define LED0_NODE DT_ALIAS(led0)
-#define BUTTON0_NODE DT_ALIAS(sw0) //sw0 name found at the top of the DT
 
 //gpio_dr_spec is another option
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON0_NODE, gpios);
+
 
 int main() {
     int ret;
     if(!gpio_is_ready_dt(&led)) {
-        return -1;
-    }
-    if(!gpio_is_ready_dt(&button)) {
         return -1;
     }
 
@@ -26,18 +22,7 @@ int main() {
         return ret;
     }
 
-    //configure input button
-    ret = gpio_pin_configure_dt(&button, GPIO_INPUT); 
-    if(ret < 0) {
-        return ret;
-    }
-
-
     while(1) {
-        //wait for the button
-        while(!gpio_pin_get_dt(&button));
-
-        //toggle
         ret = gpio_pin_toggle_dt(&led);
         if(ret < 0) {
             return ret;
